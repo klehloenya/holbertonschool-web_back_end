5093265a4cd180dba6d64217c2c098bc70c96d84
klehloenya <keketsolehloenya516@gmail.com>
2024-11-28 17:08:35 +0200

    0-constants.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,33 @@ module.exports = {
       es6: true,
       jest: true,
     },
-    extends: ["airbnb-base", "plugin:jest/all"],
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
     globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly",
-},
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
     parserOptions: {
       ecmaVersion: 2018,
-      sourceType: "module",
-},
-    plugins: ["jest"],
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
     rules: {
-      "no-console": "off",
-      "no-shadow": "off",
-      "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
-},
-    overrides: [
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
       {
-        files: ["*.js"],
-        excludedFiles: "babel.config.js",
-      },
-    ],
-};
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
+}; 
+ 
