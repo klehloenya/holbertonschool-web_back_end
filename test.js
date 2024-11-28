c55a4238dc9bfe2568c2bb9b7cf708353711269f
klehloenya <keketsolehloenya516@gmail.com>
2024-11-28 14:45:50 +0200

    0-constants.js

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-import { taskFirst, taskNext } from './0-constants.js';
+// test.js
+import { taskFirst, taskNext } from './0-constants';
 
-console.log(`${taskFirst()} ${taskNext()}`);
+test('taskFirst returns the correct string', () => {
+  expect(taskFirst()).toBe('I prefer const when I can.');
+});
+
+test('taskNext returns the correct string', () => {
+  expect(taskNext()).toBe('But sometimes let is okay');
+});
