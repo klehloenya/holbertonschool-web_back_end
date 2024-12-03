2906d7f86b124299caed8f6e0d02b68cbae3471c
klehloenya <keketsolehloenya516@gmail.com>
2024-12-03 12:06:48 +0200

    updates

--- ES6_classes/8-hbtn_classes.js
+++ ES6_classes/8-hbtn_classes.js
@@ -1,14 +1,32 @@
 export default class HolbertonClass {
     constructor(size, location) {
-      this._size = size;
-      this._location = location;
+      this.size = size;
+      this.location = location;
     }
   
-    valueOf() {
+    get size() {
       return this._size;
     }
   
-    toString() {
+    set size(value) {
+      this._size = value;
+    }
+  
+    get location() {
       return this._location;
+    }
+  
+    set location(value) {
+      this._location = value;
+    }
+  
+    [Symbol.toPrimitive](hint) {
+      if (hint === 'number') {
+        return this.size;
+      }
+      if (hint === 'string') {
+        return this.location;
+      }
+      return this;
     }
   }
