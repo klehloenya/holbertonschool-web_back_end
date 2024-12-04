17fd4037456ffe0cbe21393f1ed3be8531921fb1
klehloenya <keketsolehloenya516@gmail.com>
2024-12-04 15:42:56 +0200

    0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return their sum.
+
+    Args:
+        a (float): First number
+        b (float): Second number
+
+    Returns:
+        float: Sum of the two input numbers
+    """
+    return a + b
